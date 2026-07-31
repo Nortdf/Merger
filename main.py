a03567bc2f01aa95d18ad2a05daa4482ec74f3ba
Norto <...>
2026-07-31 22:47:22 +0300

    little changes

--- main.py
+++ main.py
@@ -3,26 +3,28 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 
-files_paths: list = []
-
 def main() -> None:
+    paths: dict = {'file1': None, 'file2': None}
+
     root = Tk()
     root.title("JustMerge")
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    ttk.Button(frm, text="Choose file", command=choose_file).grid(column=0, row=0)
-    ttk.Button(frm, text="Choose file", command=choose_file).grid(column=1, row=0)
-    ttk.Button(frm, text="Merge", command=process).grid(column=0, row=1)
+    ttk.Button(frm, text="Choose file 1", command=lambda: choose_file(paths, 'file1')).grid(column=0, row=0)
+    ttk.Button(frm, text="Choose file 2", command=lambda: choose_file(paths, 'file2')).grid(column=1, row=0)
+    ttk.Button(frm, text="Merge", command=lambda: process(paths=paths)).grid(column=0, row=1)
     root.mainloop()
 
-def choose_file() -> str:
-    files_paths.append(filedialog.askopenfilename(
+def choose_file(container: dict, name: str) -> None:
+    path = filedialog.askopenfilename(
         title="Choose file",
         filetypes=[
             ("Text files", ".txt"),
         ],
         initialdir="/"
-    ))
+    )
+    if path:
+        container[name] = path
 
 def handle_file(fp) -> list:
     try:
@@ -32,19 +34,27 @@ def handle_file(fp) -> list:
         print(f"File handle error: {e}")
         return []
 
-def process():
-    if len(files_paths) != 2:
+def process(paths: dict) -> None:
+    if paths['file1'] is None or paths['file2'] is None:
         print("Except files_paths isn't exsist")
         result = messagebox.showinfo(title="Warning!", message="You don't choose all files paths.")
     else:
-        result = merge(f1 = list(dict.fromkeys(handle_file(files_paths[0]))), f2 = list(dict.fromkeys(handle_file(files_paths[1]))))
+        f1 = list(dict.fromkeys(handle_file(paths["file1"])))
+        f2 = list(dict.fromkeys(handle_file(paths["file2"])))
+        if not f1:
+            messagebox.showinfo(title="Warning!", message="File 1 is empty")
+            return
+        if not f2:
+            messagebox.showinfo(title="Warning!", message="File 2 is empty")
+            return
+        result = merge(f1 = f1, f2 = f2)
         try: 
             with open("merged_file.txt", "w") as f:
                 f.write(result)
         except (FileExistsError, PermissionError, UnicodeDecodeError) as e:
             print(f"File merge error: ")
 
-def merge(f1: list, f2: list) -> str:
+def merge(f1: list, f2: list):
     f1[len(f1)-1] = f1[len(f1)-1] + "\n"
     f2[len(f2)-1] = f2[len(f2)-1] + "\n"
     return "".join(list(dict.fromkeys(list(f1 + f2))))
